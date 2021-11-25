2fb1eb8079597fa38905a7a5ed92969ed37baf62
yagodurcak <mauridurcak@gmail.com>
2021-11-25 11:46:04 -0300

    finish login 12511 1145

--- country/src/components/Login.js
+++ country/src/components/Login.js
@@ -1,27 +1,63 @@
-import Container from "@mui/material/Container"
+import "./Login.css"
+
+import Button from "@mui/material/Container"
 import React from 'react';
 import imagen from "../IMG/Groupicono.png"
 import styled from '@emotion/styled';
 
-const Title= styled.h1`
-    font-size: 20px;
-    color: red;
+const Containers= styled.div`
+ /* background-color: red; */
+ display: grid;
+ justify-content: center;
+ align-items: center;
 `
 const Icono = styled.img`
     height: 200px;
-    width: 200px;
+    width: 220px;
+    margin: 10px 10px;
     
 `
 
+const Input = styled.input`
+    height: 70px;
+    width: 500px;
+    background-color: transparent;
+    border: 1px solid white;
+    border-radius: 10px;
+    padding-left: 10px;
+    color: white;
+    font-size: 20px;
+    text-align: center;   
+
+`
+
+
 function Login() {
     return (
-        <Container>
-            <div>
-                <Icono src={imagen} alt="" />
-                <h1>asasd</h1>
-            </div>
+     
+            <Containers>
+                <Containers>
+                    <Icono src={imagen} alt="" />
+                </Containers>
+                <form action="" className="mt-10">
+                    <div className="inputContainer">
+                        <i class="fa fa-user fa-2x icon"> </i>                       
+                        <input type="text" placeholder="USERNAME"  className="mt-5 placeholder-white Field"/>
+                    </div>
+                    <div className="inputContainer">
+                        <i class="fa fa-lock fa-2x icon"> </i>                       
+                        <input type="text" placeholder="PASSWORD"  className="mt-5 placeholder-white Field"/>
+                    </div>
+                    <button className="boton">LOGIN</button>
+                    <div className="grid justify-items-end mt-5">
+                        <a href=""><h3 className="text-white forgot">Forgot password? </h3></a>
+                    </div>
+        
+                </form>
+                
+            </Containers>
 
-        </Container>
+      
     )
 }
 
